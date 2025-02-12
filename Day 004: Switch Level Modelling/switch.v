99da07ed39027b6b4bd8b1fa72737b2e2688eefc
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-13 00:21:27 +0530

    Update switch.v

--- Day 004: Switch Level Modelling/switch.v
+++ Day 004: Switch Level Modelling/switch.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/12/2025 11:09:55 PM
-// Design Name: 
-// Module Name: switch
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module switch(
     input in, //input is said to be in

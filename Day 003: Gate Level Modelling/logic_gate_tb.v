34618f65c3bd1558a5ad11aa685504ed6c799da6
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-12 19:56:49 +0530

    Add files via upload

---
+++ Day 003: Gate Level Modelling/logic_gate_tb.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/11/2025 12:05:29 PM
+// Design Name: 
+// Module Name: logic_gate_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+// we will be using this testbench to simulate the waveforms.
+module logic_gate_tb();
+    reg A,B; //here, inputs are reg types, they are used to hold and assign values to these signals during the simulation
+    wire andg, //outputs, are wire because they represent signals that are driven by the module under test (DUT)
+         org,
+         notg,
+         nandg,
+         norg,
+         xorg,
+         xnorg;
+         
+     logic_gates DUT(.a(A),.b(B),.and_gate(andg),.or_gate(org),.not_gate(notg),.nand_gate(nandg),.nor_gate(norg),.xor_gate(xorg),.xnor_gate(xnorg));
+     //this is how we represent Instantiation if variables are different.
+     // we can arrange them in any order of our choice, just use '.' operator
+     initial  // this is used to run the simulation just once.
+         begin // this is used with end. used to group two or more assignments (that are given below).
+             #10 A= 1'b0; B= 1'b0; // simulation sstarts from 0, upto 10 A=0,B=0
+             #10 A= 1'b0; B= 1'b1; // from 10 to 20, A=0,b=1
+             #10 A= 1'b1; B= 1'b0; // from 20 to 30, A=1,B=0
+             #10 A= 1'b1; B= 1'b1; // from 30 to 40, A=1,B=1
+         end
+endmodule
---
+++ Day 003: Gate Level Modelling/logic_gates.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/11/2025 12:05:12 PM
+// Design Name: 
+// Module Name: logic_gates
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+//we will be implementing the basic logic gates using gate level modelling
+
+module logic_gates(        
+    input a,b,             // taking 2 inputs, a and b
+    output and_gate,   // taking and_gate as output of AND operation
+    output or_gate,    // taking or_gate as output of OR operation
+    output not_gate,   // taking not_gate as output of NOT operation
+    output nand_gate,  // taking nand_gate as output of NAND operation
+    output nor_gate,   // taking nor_gate as output of NOR operation
+    output xor_gate,   // taking xor_gate as output of XOR operation
+    output xnor_gate   // taking xnor_gate as output of XNOR operation
+    );
+    
+//why didn't we use reg type as output?
+// output signals in gate level modelling are generally done using pre-defined gate keywords.
+    
+    and andgate(and_gate, a, b);
+    or orgate(or_gate, a, b);
+    not notgate(not_gate, a);
+    nand nandgate(nand_gate, a, b);
+    nor norgate(nor_gate, a, b);
+    xor xorgate(xor_gate, a, b);
+    xnor xnorgate(xnor_gate, a, b);
+    
+endmodule

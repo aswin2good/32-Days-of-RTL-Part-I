c86dae351a555e8f15737d45e894caee8c359b89
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-12 19:58:22 +0530

    Update logic_gate_tb.v

--- Day 002: Structural Modelling/logic_gate_tb.v
+++ Day 002: Structural Modelling/logic_gate_tb.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/11/2025 11:57:05 AM
-// Design Name: 
-// Module Name: logic_gate_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 // we will be using this testbench to simulate the waveforms.
 module logic_gate_tb();

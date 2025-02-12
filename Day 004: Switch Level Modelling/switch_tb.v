77b3f3641e4f184b5075b2a1c99356a551a522b5
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-13 00:21:56 +0530

    Update switch_tb.v

--- Day 004: Switch Level Modelling/switch_tb.v
+++ Day 004: Switch Level Modelling/switch_tb.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/12/2025 11:27:17 PM
-// Design Name: 
-// Module Name: switch_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module switch_tb();
     reg in;  

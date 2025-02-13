48af8bee1634c9d8ab6e5663d082d25019e55f45
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-13 07:46:28 +0530

    Update univ_nand.v

--- Day 005: Universal Gate Switch Level Modelling/NAND Gate/univ_nand.v
+++ Day 005: Universal Gate Switch Level Modelling/NAND Gate/univ_nand.v
@@ -1,36 +1,18 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/12/2025 11:46:28 PM
-// Design Name: 
-// Module Name: univ_nand
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module univ_nand(
-    input a,b,  
-    output out
+    input a,b,  //let inputs have two values a and b
+    output out //let output be said to be out, and by default it is of wire type.
     );
     
-    supply0 gnd;
-    supply1 vdd;
-    wire someV; 
+    supply0 gnd; //supply0 is a net type that means logic 0 / ground.
+    supply1 vdd; //supply1 is a net type that means logic 1 / voltage supply
+    wire someV; //we are taking some temporary value of voltage as it acts as a common unknown connection between nmos. 
     
-    nmos(someV,gnd,b);
+    nmos(someV,gnd,b); //nmos is a predefined primitive keyword: nmos (output(drain), input(source), control(gate));
     nmos(out,someV,a);
-    pmos(out,vdd,a);
-    pmos(out,vdd,b);
     
+    pmos(out,vdd,a);//pmos is a predefined primitive keyword: pmos (output(drain), input(source), control(gate));
+    pmos(out,vdd,b);
+
+    // nmos and pmos are the keywords generally used in a switch level modelling. 
 endmodule

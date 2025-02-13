b8db32ecc03dd147102e8ced02468ade3324c443
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-13 07:49:04 +0530

    Update univ_nor.v

--- Day 005: Universal Gate Switch Level Modelling/NOR Gate/univ_nor.v
+++ Day 005: Universal Gate Switch Level Modelling/NOR Gate/univ_nor.v
@@ -1,38 +1,18 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/12/2025 11:46:28 PM
-// Design Name: 
-// Module Name: univ_nor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module univ_nor(
-    input a,b, 
-    output out
+    input a,b, //inputs are let us say a and b
+    output out //let output be out, and is of wire type
     );
     
-    supply0 gnd;
-    supply1 vdd;
-    wire someV;
+    supply0 gnd; //supply0 is a net type that means logic 0 / ground.
+    supply1 vdd; //supply1 is a net type that means logic 1 / voltage supply.
+    wire someV; //we are taking some temporary value of voltage as it acts as a common unknown connection between nmos.
     
-    nmos(out,gnd,a);
+    nmos(out,gnd,a); //nmos is a predefined primitive keyword: nmos (output(drain), input(source), control(gate));
     nmos(out,gnd,b);
-    pmos(someV,vdd,a);
+    
+    pmos(someV,vdd,a); //pmos is a predefined primitive keyword: pmos (output(drain), input(source), control(gate));
     pmos(out,someV,b);
     
-    
+    // nmos and pmos are the keywords generally used in a switch level modelling. 
 endmodule

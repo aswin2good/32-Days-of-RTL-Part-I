32fc0debf0b65c5b192131261845d871bc776c12
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-13 07:51:02 +0530

    Update nor_tb.v

--- Day 005: Universal Gate Switch Level Modelling/NOR Gate/nor_tb.v
+++ Day 005: Universal Gate Switch Level Modelling/NOR Gate/nor_tb.v
@@ -1,35 +1,15 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/13/2025 12:06:13 AM
-// Design Name: 
-// Module Name: nor_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module nor_tb();
-    reg Vin1, Vin2;
-    wire Vout;
+    reg Vin1, Vin2; //inputs are reg type, because inputs' value will be used to find out the value of output.
+    wire Vout; //output are wire type, because it has to change continously by DUT.
     
-    univ_nor DUT(.a(Vin1),.b(Vin2), .out(Vout));
+    univ_nor DUT(.a(Vin1),.b(Vin2), .out(Vout)); //instantiation
     
-    initial Vin1= 1'b0;
-    initial Vin2= 1'b0;
-    initial forever #10 Vin1 = ~Vin1;
-    initial forever #20 Vin2 = ~Vin2;
-    initial #200 $finish;
-    
+    initial Vin1= 1'b0; //initializing value of Vin1 as 0
+    initial Vin2= 1'b0; //initializing value of Vin2 as 0
+    initial forever #10 Vin1 = ~Vin1; // intializing and repeating this command of changing in value every 10ns.
+    initial forever #20 Vin2 = ~Vin2; // intializing and repeating this command of changing in value every 20ns.
+    initial #200 $finish; // intializing to finish this testbench at t=100ns
+
+        //NOTE: all initial statements run together when the code is executed, and not one after the other(linewise).
 endmodule

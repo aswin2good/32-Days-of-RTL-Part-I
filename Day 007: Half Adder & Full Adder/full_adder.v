ae4ced844c66bbcb21ef1f08a08ee8f67d41c0e6
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-15 21:24:54 +0530

    Update full_adder.v

--- Day 007: Half Adder & Full Adder/full_adder.v
+++ Day 007: Half Adder & Full Adder/full_adder.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/15/2025 08:14:48 PM
-// Design Name: 
-// Module Name: full_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 //we will be implementing full adder design module
 module full_adder(

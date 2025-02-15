971ba173fb593fa142cd5575567efda07bf9dd92
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-15 19:36:02 +0530

    Update odd_parity_generate.v

--- Day 006: Parity Generator & Checker/Odd Parity/odd_parity_generate.v
+++ Day 006: Parity Generator & Checker/Odd Parity/odd_parity_generate.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/15/2025 06:57:27 PM
-// Design Name: 
-// Module Name: odd_parity_generate
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 
 module odd_parity_generate(
     input wire [7:0] data_in, // data_in is a 8 bit input, and is of type wire.

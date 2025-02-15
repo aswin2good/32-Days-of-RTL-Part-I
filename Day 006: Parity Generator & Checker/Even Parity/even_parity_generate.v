cad8ead8e003cb4a4ee7215d8c542219df14a7ba
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-15 19:38:35 +0530

    Update even_parity_generate.v

--- Day 006: Parity Generator & Checker/Even Parity/even_parity_generate.v
+++ Day 006: Parity Generator & Checker/Even Parity/even_parity_generate.v
@@ -1,25 +1,6 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/13/2025 01:30:14 PM
-// Design Name: 
-// Module Name: even_parity_generate
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
+//we will be implementing even parity generating design module
 module even_parity_generate(
     input wire [7:0] data_in, // data_in is a 8 bit input, and is of type wire.
     output wire parity        // parity is a single bit, is also a wire type

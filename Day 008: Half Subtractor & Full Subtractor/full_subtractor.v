060494bedaec8b6db0edadcb701219dfdf633fdd
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-16 19:30:03 +0530

    Update full_subtractor.v

--- Day 008: Half Subtractor & Full Subtractor/full_subtractor.v
+++ Day 008: Half Subtractor & Full Subtractor/full_subtractor.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/16/2025 06:51:44 PM
-// Design Name: 
-// Module Name: full_subtractor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 //we will be implementing full subtractor design module
 module full_subtractor(

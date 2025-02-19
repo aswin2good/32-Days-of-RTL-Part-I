bdb57319170165ab4ab7d6c24ec6953d9107a4ee
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-19 10:17:13 +0530

    Update half_subtractor_tb.v

--- Day 010: Adders & Subtractors using NAND Gates/half_subtractor_tb.v
+++ Day 010: Adders & Subtractors using NAND Gates/half_subtractor_tb.v
@@ -1,30 +1,12 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/18/2025 07:52:19 AM
-// Design Name: 
-// Module Name: half_subtractor_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-/*
+//this is the testbench for half subtractor design module
 module half_subtractor_tb();
     reg a,b; 
     wire Diff, Bout;
     
-    half_subtractor_nand DUT(a,b,Diff,Bout);
+    half_subtractor_nand DUT(a,b,Diff,Bout); //instantiating half subtractor design module
+    
     initial begin
     #10 a=1'b0; b=1'b0; 
     #10 a=1'b0; b=1'b1;
@@ -34,4 +16,3 @@ module half_subtractor_tb();
     end
     
 endmodule
-*/

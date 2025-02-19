5d3d835bcfa23c3bd713d9374a68a4e918e6893e
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-19 10:13:10 +0530

    Update full_adder_tb.v

--- Day 010: Adders & Subtractors using NAND Gates/full_adder_tb.v
+++ Day 010: Adders & Subtractors using NAND Gates/full_adder_tb.v
@@ -1,30 +1,10 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/18/2025 08:00:47 AM
-// Design Name: 
-// Module Name: full_adder_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
+//this is the testbench for the full adder design module
 module full_adder_tb();
     reg a,b,cin;
     wire Sum, Cout;
     
-    full_adder_nand DUT(a,b,cin,Sum,Cout);
+    full_adder_nand DUT(a,b,cin,Sum,Cout);//instantiating full adder design module
     
     initial begin
     #10 a=1'b0 ; b=1'b0; cin=1'b0;

cd9f8161cc446a176b35d1681af3b7885b312f20
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-19 11:10:18 +0530

    Update adder_subtractor.v

--- Day 011: 4-bit Adder & Subtractor/adder_subtractor.v
+++ Day 011: 4-bit Adder & Subtractor/adder_subtractor.v
@@ -1,8 +1,9 @@
 `timescale 1ns / 1ps
 
+//we will be implementing full adder design module
 module full_adder(
-    input a, b, cin,
-    output Sout,Cout
+    input a, b, cin, //taking three inputs
+    output Sout,Cout //taking two outputs, sum and carry
     );
     
     assign Sout= a^b^cin; 
@@ -18,7 +19,7 @@ module adder_subtractor(
     );
     wire [2:0]c; //temporary carry 
     
-    full_adder fa1(A[0], (B[0]^en), en, sum_or_diff[0], c[0]);
+    full_adder fa1(A[0], (B[0]^en), en, sum_or_diff[0], c[0]);  //XOR B and en we entered.
     full_adder fa2(A[1], (B[1]^en), c[0], sum_or_diff[1], c[1]);
     full_adder fa3(A[2], (B[2]^en), c[1], sum_or_diff[2], c[2]);
     full_adder fa4(A[3], (B[3]^en), c[2], sum_or_diff[3], cout_or_bout);

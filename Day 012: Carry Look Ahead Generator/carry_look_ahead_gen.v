0c48d77e4c4e16025f8d8e157b2edbf7b654a54d
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-20 23:44:16 +0530

    Update carry_look_ahead_gen.v

--- Day 012: Carry Look Ahead Generator/carry_look_ahead_gen.v
+++ Day 012: Carry Look Ahead Generator/carry_look_ahead_gen.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/20/2025 07:36:04 AM
-// Design Name: 
-// Module Name: carry_look_ahead_gen
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 //we will be implementing carry look ahead adder using carry and propagate

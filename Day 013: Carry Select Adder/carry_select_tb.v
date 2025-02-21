40c32afb1452d0eae44793550b90ddf6ea123f71
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-21 08:35:29 +0530

    Update carry_select_tb.v

--- Day 013: Carry Select Adder/carry_select_tb.v
+++ Day 013: Carry Select Adder/carry_select_tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/21/2025 07:33:04 AM
-// Design Name: 
-// Module Name: carry_select_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module carry_select_tb();
     reg [3:0]a,b;

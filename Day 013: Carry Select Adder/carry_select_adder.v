7352d4baf5229b890e22918c7eb897009dffb510
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-21 08:34:30 +0530

    Update carry_select_adder.v

--- Day 013: Carry Select Adder/carry_select_adder.v
+++ Day 013: Carry Select Adder/carry_select_adder.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/21/2025 07:12:45 AM
-// Design Name: 
-// Module Name: carry_select_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module full_adder(
     input a, b, cin,

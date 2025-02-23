ae7c676072c2ad9fd78fabdc26d50e43b6043e3f
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-23 13:06:14 +0530

    Update multiplier_tb.v

--- Day 015: 4 bit Multiplier/multiplier_tb.v
+++ Day 015: 4 bit Multiplier/multiplier_tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/23/2025 12:39:36 PM
-// Design Name: 
-// Module Name: multiplier_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 //this is the testbench for multiplier design module
 module multiplier_tb();

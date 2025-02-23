afd9083cb6b18245e12bac7146bec8e0d0a6cd74
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-23 13:06:03 +0530

    Update multiplier_4_bit.v

--- Day 015: 4 bit Multiplier/multiplier_4_bit.v
+++ Day 015: 4 bit Multiplier/multiplier_4_bit.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/23/2025 10:01:27 AM
-// Design Name: 
-// Module Name: multiplier_4_bit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 //we will implement a 4 bit multiplier design module
 module multiplier_4_bit(

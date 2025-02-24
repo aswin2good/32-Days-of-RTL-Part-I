5bbaccd4eaa37922c059473119c161bd6ec00511
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-24 07:33:37 +0530

    Update divider_4_bit.v

--- Day 016: 4 bit Divider/divider_4_bit.v
+++ Day 016: 4 bit Divider/divider_4_bit.v
@@ -1,28 +1,9 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/24/2025 07:13:51 AM
-// Design Name: 
-// Module Name: divider_4_bit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
+//we will implement 4 bit divider design 
 module divider_4_bit(
-    input [3:0] dividend,divisor,
-    output reg [3:0] remainder,quotient
+    input [3:0] dividend,divisor,       //4bit dividend and divisor
+    output reg [3:0] remainder,quotient //4bit remainder and quotient
     );
     
     always@(dividend,divisor)

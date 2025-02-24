617d8f54c44e35f0c2d832125267d8cf21504cee
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-24 07:34:44 +0530

    Update divider_tb.v

--- Day 016: 4 bit Divider/divider_tb.v
+++ Day 016: 4 bit Divider/divider_tb.v
@@ -1,35 +1,16 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/24/2025 07:24:21 AM
-// Design Name: 
-// Module Name: divider_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
+//this is the testbench for divider module
 module divider_tb();
     reg [3:0]dividend,divisor;
     wire [3:0]quotient,remainder;
 
-    divider_4_bit DUT (dividend,divisor,quotient,remainder);
+	divider_4_bit DUT (dividend,divisor,quotient,remainder);  //instantiating divider design module
 		always begin
 			
 			dividend =$random;
 			divisor =$random;
-            #10;
+                        #10;
 		end
 		initial begin #90 $finish;
 		end

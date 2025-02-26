dc7fa4b8053a9f6192e59ca694538ce12edb6192
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-02-26 11:32:31 +0530

    Update mux_4to1.v

--- Day 019: 4x1 MUX/mux_4to1.v
+++ Day 019: 4x1 MUX/mux_4to1.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/26/2025 08:43:28 AM
-// Design Name: 
-// Module Name: mux_4to1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module mux_4to1(

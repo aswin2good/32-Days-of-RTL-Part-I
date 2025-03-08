32cb7cf983069e38254bb1148da56fc82150e578
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-03-08 12:18:48 +0530

    Update n_bit_tb.v

--- Day 027: N-bit Square Number/n_bit_tb.v
+++ Day 027: N-bit Square Number/n_bit_tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/08/2025 11:47:35 AM
-// Design Name: 
-// Module Name: n_bit_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 //this is the testbench for n bit square design module
 module n_bit_tb();

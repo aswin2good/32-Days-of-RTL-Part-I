5e8f06cf161473861e84837d74c5a9fdf69fc6ec
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-03-11 19:24:00 +0530

    Update binary_to_2s.v

--- Day 030: Binary to 2's Complement/binary_to_2s.v
+++ Day 030: Binary to 2's Complement/binary_to_2s.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/11/2025 07:10:30 PM
-// Design Name: 
-// Module Name: binary_to_2s
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 //we will implement binary to 2s complement conversion design module
 module binary_to_2s(

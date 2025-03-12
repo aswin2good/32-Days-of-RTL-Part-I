ff237f05926c2bce20b02b6d48f6f9feca05083a
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-03-12 20:46:57 +0530

    Update binary_to_bcd.v

--- Day 031: Binary to BCD Conversion/binary_to_bcd.v
+++ Day 031: Binary to BCD Conversion/binary_to_bcd.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/12/2025 08:27:38 PM
-// Design Name: 
-// Module Name: binary_to_bcd
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 //we will implement a binary to BCD conversion design module
 module binary_to_bcd(
